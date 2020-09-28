d8c672aa504a23551575259f6e6da4d10aa67172
vincenterc <chung.menghsun@gmail.com>
2020-09-28 21:47:31 +0800

    Init

---
+++ desktop/src/com/vincenterc/libgdxtest/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.vincenterc.libgdxtest.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.vincenterc.libgdxtest.TestGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(TestGame(), config)
+    }
+}

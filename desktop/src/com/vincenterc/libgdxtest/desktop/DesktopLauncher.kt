e792ab8628599d42c1f71f8bb8a0af79b0232fc3
vincenterc <chung.menghsun@gmail.com>
2020-09-28 22:02:51 +0800

    Use lwjgl3 backend

--- desktop/src/com/vincenterc/libgdxtest/desktop/DesktopLauncher.kt
+++ desktop/src/com/vincenterc/libgdxtest/desktop/DesktopLauncher.kt
@@ -2,12 +2,17 @@ package com.vincenterc.libgdxtest.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.vincenterc.libgdxtest.TestGame
 
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration()
-        LwjglApplication(TestGame(), config)
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setTitle("libGDX-test-game")
+        config.setWindowedMode(800, 600)
+
+        Lwjgl3Application(TestGame(), config)
     }
 }

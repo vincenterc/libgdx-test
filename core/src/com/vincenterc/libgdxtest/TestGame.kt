87a9a58467e15602a5954fd9594a5fd1698d6d49
vincenterc <chung.menghsun@gmail.com>
2020-09-29 16:47:59 +0800

    Add the MenuScreen and buttons for switching screens

--- core/src/com/vincenterc/libgdxtest/TestGame.kt
+++ core/src/com/vincenterc/libgdxtest/TestGame.kt
@@ -1,17 +1,21 @@
 package com.vincenterc.libgdxtest
 
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.vincenterc.libgdxtest.screens.DefaultScreen
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.vincenterc.libgdxtest.screens.MenuScreen
 
 class TestGame : Game() {
 
     lateinit var batch: SpriteBatch
+    lateinit var skin:Skin
 
     override fun create() {
         batch = SpriteBatch()
+        skin = Skin(Gdx.files.internal("skin/uiskin.json"))
 
-        setScreen(DefaultScreen(this))
+        setScreen(MenuScreen(this))
     }
 
     override fun dispose() {
--- core/src/com/vincenterc/libgdxtest/screens/MenuScreen.kt
+++ core/src/com/vincenterc/libgdxtest/screens/MenuScreen.kt
@@ -1,53 +1,46 @@
 package com.vincenterc.libgdxtest.screens
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.vincenterc.libgdxtest.TestGame
 
-class DefaultScreen(var game: TestGame) : ScreenAdapter() {
-
-    private var stage = Stage(ScreenViewport())
-    private lateinit var img: Texture
+class MenuScreen(var game: TestGame) : ScreenAdapter() {
+    var stage = Stage(ScreenViewport())
 
     override fun show() {
         Gdx.input.inputProcessor = stage
 
-        var menuButton = TextButton("Menu", game.skin, "default")
-        menuButton.setPosition(
-            Gdx.graphics.width - menuButton.width - 10f,
-            Gdx.graphics.height - menuButton.height - 10f
+        var defaultScreenButton = TextButton("Default Screen", game.skin, "default")
+        defaultScreenButton.setPosition(
+            Gdx.graphics.width / 2f - defaultScreenButton.width / 2f,
+            Gdx.graphics.height / 2f
         )
-        menuButton.addListener(object : InputListener() {
+        defaultScreenButton.addListener(object : InputListener() {
             override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
-                game.screen = MenuScreen(game)
+                game.screen = DefaultScreen(game)
                 return true
             }
         })
-        stage.addActor(menuButton)
-
-        img = Texture("badlogic.jpg")
+        stage.addActor(defaultScreenButton)
     }
 
     override fun render(delta: Float) {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(Color.LIGHT_GRAY.r, Color.LIGHT_GRAY.g, Color.LIGHT_GRAY.b, Color.LIGHT_GRAY.a)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-
-        game.batch.begin()
-        game.batch.draw(img, 0f, 0f)
-        game.batch.end()
 
         stage.act()
         stage.draw()
     }
 
     override fun hide() {
-        img.dispose()
+        Gdx.input.inputProcessor = null
     }
 }

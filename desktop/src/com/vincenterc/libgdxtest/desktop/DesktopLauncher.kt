8c98cc53c4b8168a683f1af5f64234aa443b078d
vincenterc <chung.menghsun@gmail.com>
2020-09-29 20:50:22 +0800

    Set window size to default for desktop

--- desktop/src/com/vincenterc/libgdxtest/desktop/DesktopLauncher.kt
+++ desktop/src/com/vincenterc/libgdxtest/desktop/DesktopLauncher.kt
@@ -11,7 +11,6 @@ object DesktopLauncher {
     fun main(arg: Array<String>) {
         val config = Lwjgl3ApplicationConfiguration()
         config.setTitle("libGDX-test-game")
-        config.setWindowedMode(800, 600)
 
         Lwjgl3Application(TestGame(), config)
     }

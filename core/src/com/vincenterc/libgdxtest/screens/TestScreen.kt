580acfc2567d513004057de27eddc0c61a3c92e7
vincenterc <chung.menghsun@gmail.com>
2020-10-03 21:31:08 +0800

    Use only one viewport in TestScreen

--- core/src/com/vincenterc/libgdxtest/screens/TestScreen.kt
+++ core/src/com/vincenterc/libgdxtest/screens/TestScreen.kt
@@ -3,24 +3,17 @@ package com.vincenterc.libgdxtest.screens
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
-import com.badlogic.gdx.utils.viewport.StretchViewport
 import com.vincenterc.libgdxtest.TestGame
 
 class TestScreen(var game: TestGame) : BaseScreen() {
 
-    private var viewport = StretchViewport(640f, 480f)
-    private var batch = SpriteBatch()
     private var img = Texture(Gdx.files.internal("badlogic.jpg"))
 
     override fun show() {
         super.show()
-
-        // Center camera
-        viewport.camera.position.set(viewport.worldWidth / 2f, viewport.worldHeight / 2f, 0f)
 
         val menuButton = TextButton("Menu", game.skin)
         menuButton.setPosition(
@@ -36,25 +29,16 @@ class TestScreen(var game: TestGame) : BaseScreen() {
         stage.addActor(menuButton)
     }
 
-    override fun resize(width: Int, height: Int) {
-        super.resize(width, height)
-
-        viewport.update(width, height)
-    }
-
     override fun render(delta: Float) {
         super.render(delta)
 
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        viewport.apply()
-        batch.projectionMatrix = viewport.camera.combined
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        stage.batch.begin()
+        stage.batch.draw(img, 0f, 0f)
+        stage.batch.end()
 
-        stage.viewport.apply()
         stage.draw()
     }
 

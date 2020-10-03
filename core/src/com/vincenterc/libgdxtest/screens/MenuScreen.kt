f2eeb7cf0de83f339e7d8482aa01c5a1f315d31a
vincenterc <chung.menghsun@gmail.com>
2020-10-03 21:50:01 +0800

    Use Table in MenuScreen

--- core/src/com/vincenterc/libgdxtest/screens/MenuScreen.kt
+++ core/src/com/vincenterc/libgdxtest/screens/MenuScreen.kt
@@ -2,6 +2,7 @@ package com.vincenterc.libgdxtest.screens
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
+import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.vincenterc.libgdxtest.TestGame
 
@@ -11,10 +12,6 @@ class MenuScreen(var game: TestGame) : BaseScreen() {
         super.show()
 
         val testScreenButton = TextButton("Test Screen", game.skin)
-        testScreenButton.setPosition(
-            stage.width / 2f - testScreenButton.width / 2f,
-            stage.height / 2f - testScreenButton.height / 2f
-        )
         testScreenButton.addListener(object : InputListener() {
             override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
                 game.screen = TestScreen(game)
@@ -24,10 +21,6 @@ class MenuScreen(var game: TestGame) : BaseScreen() {
         stage.addActor(testScreenButton)
 
         val twoViewportsScreenButton = TextButton("Two Viewports Screen", game.skin)
-        twoViewportsScreenButton.setPosition(
-            stage.width / 2f - twoViewportsScreenButton.width / 2f,
-            stage.height / 2f - twoViewportsScreenButton.height / 2f - 30f
-        )
         twoViewportsScreenButton.addListener(object : InputListener() {
             override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
                 game.screen = TwoViewportsScreen(game)
@@ -35,5 +28,14 @@ class MenuScreen(var game: TestGame) : BaseScreen() {
             }
         })
         stage.addActor(twoViewportsScreenButton)
+
+        val table = Table()
+        table.defaults().fillX()
+        table.add(testScreenButton).spaceBottom(10f)
+        table.row()
+        table.add(twoViewportsScreenButton)
+        table.setFillParent(true)
+//        table.debug = true
+        stage.addActor(table)
     }
 }

564ca7aa24f01d464a3f93b08034c0554ef33796
vincenterc <chung.menghsun@gmail.com>
2020-10-04 17:13:09 +0800

    Use utils.OrthoCamController in CameraControlScreen

--- core/src/com/vincenterc/libgdxtest/screens/CameraControlScreen.kt
+++ core/src/com/vincenterc/libgdxtest/screens/CameraControlScreen.kt
@@ -10,7 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.utils.viewport.FitViewport
-import com.vincenterc.libgdxtest.OrthoCamController
+import com.vincenterc.libgdxtest.utils.OrthoCamController
 import com.vincenterc.libgdxtest.TestGame
 
 class CameraControlScreen(var game: TestGame) : BaseScreen() {

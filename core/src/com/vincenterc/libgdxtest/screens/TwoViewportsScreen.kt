1ab296974bd86a25fca96d48ee5066fb395e30b3
vincenterc <chung.menghsun@gmail.com>
2020-10-08 02:18:53 +0800

    Remove unnecessary code

--- core/src/com/vincenterc/libgdxtest/screens/TwoViewportsScreen.kt
+++ core/src/com/vincenterc/libgdxtest/screens/TwoViewportsScreen.kt
@@ -29,9 +29,6 @@ class TwoViewportsScreen(var game: TestGame) : BaseScreen() {
         )
         menuButton.addListener(object : InputListener() {
             override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
-
-                println("touchDown....")
-
                 game.screen = MenuScreen(game)
                 return true
             }

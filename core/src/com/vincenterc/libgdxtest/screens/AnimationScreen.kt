21dec559e57cc1c8240808dc33dce1e399bff10a
vincenterc <chung.menghsun@gmail.com>
2020-10-26 03:27:08 +0800

    Add a second animation that you can control by left and right keys in the AnimationScreen

--- core/src/com/vincenterc/libgdxtest/screens/AnimationScreen.kt
+++ core/src/com/vincenterc/libgdxtest/screens/AnimationScreen.kt
@@ -1,6 +1,7 @@
 package com.vincenterc.libgdxtest.screens
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Animation
@@ -14,7 +15,9 @@ class AnimationScreen(game: TestGame) : BaseScreen(game) {
     private val frameCols = 6
     private val runningSheet = Texture("running.png")
     private val runningAnimation: Animation<TextureRegion>
-    private var elapsedAnimationTime = 0f
+    private var animationTimer = 0f
+    private var animationTimer2 = 0f
+    private var animation2Scale = 1f
 
     init {
         val runningSheetRegions = TextureRegion.split(
@@ -34,13 +37,33 @@ class AnimationScreen(game: TestGame) : BaseScreen(game) {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        elapsedAnimationTime += Gdx.graphics.deltaTime
-        val currentRunningAnimationFrame = runningAnimation.getKeyFrame(elapsedAnimationTime, true)
+        animationTimer += Gdx.graphics.deltaTime
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            animationTimer2 += Gdx.graphics.deltaTime
+            animation2Scale = 1f
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            animationTimer2 += Gdx.graphics.deltaTime
+            animation2Scale = -1f
+        }
+
+        val currentRunningAnimationFrame = runningAnimation.getKeyFrame(animationTimer, true)
+        val currentRunningAnimation2Frame = runningAnimation.getKeyFrame(animationTimer2, true)
+
         stage.batch.begin()
         stage.batch.draw(
             currentRunningAnimationFrame,
             stage.viewport.worldWidth / 2f - currentRunningAnimationFrame.regionWidth / 2f,
-            stage.viewport.worldHeight / 2f - currentRunningAnimationFrame.regionHeight / 2f
+            stage.viewport.worldHeight / 2f + currentRunningAnimationFrame.regionHeight / 4f
+        )
+        stage.batch.draw(
+            currentRunningAnimation2Frame,
+            stage.viewport.worldWidth / 2f - currentRunningAnimation2Frame.regionWidth / 2f,
+            stage.viewport.worldHeight / 2f - currentRunningAnimation2Frame.regionHeight * 5 / 4f,
+            currentRunningAnimation2Frame.regionWidth / 2f, currentRunningAnimation2Frame.regionHeight / 2f,
+            currentRunningAnimation2Frame.regionWidth.toFloat(), currentRunningAnimation2Frame.regionHeight.toFloat(),
+            animation2Scale, 1f, 0f
         )
         stage.batch.end()
 

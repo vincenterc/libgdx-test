8efd6b1115288296d1f87b7dd4e620de567754ab
vincenterc <chung.menghsun@gmail.com>
2020-11-20 17:22:52 +0800

    Add Bob class

---
+++ core/src/com/vincenterc/libgdxtest/platformer/Bob.kt
@@ -0,0 +1,28 @@
+package com.vincenterc.libgdxtest.platformer
+
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.math.Vector2
+
+class Bob(var position: Vector2) {
+
+    enum class State {
+        IDLE, WALKING, JUMPING, DYING
+    }
+
+    companion object {
+        const val SPEED = 2f // unit per second
+        const val JUMP_VELOCITY = 1f
+        const val SIZE = 0.5f // half a unit
+    }
+
+    var acceleration = Vector2()
+    var velocity = Vector2()
+    var bounds = Rectangle()
+    var state = State.IDLE
+    var facingLeft = true
+
+    init {
+        bounds.width = SIZE
+        bounds.height = SIZE
+    }
+}
